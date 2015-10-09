f2f06439d9d29790b4b43854bff032701856383b
Phil Johnston <support@mintplugins.com>
2015-10-08 22:16:05 -0400

    1.0.1.1 Version Bump

--- mp-stacks-widgets.php
+++ mp-stacks-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Stacks + Widgets
 Plugin URI: http://mintplugins.com/plugins/mp-stacks-widgets
 Description: Show widgets on any page, at any time, anywhere on your website. Just put make a brick using “MP Stacks”, put the stack on a page, and set the brick’s Content-Type to be “Widgets”.
-Version: 1.0.1.0
+Version: 1.0.1.1
 Author: Mint Plugins
 Author URI: http://mintplugins.com
 Text Domain: mp_stacks_widgets
@@ -11,7 +11,7 @@ Domain Path: languages
 License: GPL2
 */
 
-/*  Copyright 2014  Phil Johnston  (email : phil@mintplugins.com)
+/*  Copyright 2015  Phil Johnston  (email : phil@mintplugins.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -34,7 +34,7 @@ License: GPL2
 */
 // Plugin version
 if( !defined( 'MP_STACKS_WIDGETS_VERSION' ) )
-	define( 'MP_STACKS_WIDGETS_VERSION', '1.0.1.0' );
+	define( 'MP_STACKS_WIDGETS_VERSION', '1.0.1.1' );
 
 // Plugin Folder URL
 if( !defined( 'MP_STACKS_WIDGETS_PLUGIN_URL' ) )
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://mintplugins.com/
 Tags: message bar, header
 Requires at least: 3.5
 Tested up to: 4.3
-Stable tag: 1.0.1.0
+Stable tag: 1.0.1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -32,6 +32,10 @@ See full instructions at http://mintplugins.com/doc/mp-stacks
 
 
 == Changelog ==
+
+= 1.0.1.1 = October 8, 2015
+* Better defaults for widgets
+* Change how PHP Session is used and created
 
 = 1.0.1.0 = September 17, 2015
 * Make widget ajax in admin fire on "mp_stacks_content_type_change_complete" instead of "mp_stacks_content_type_change"
