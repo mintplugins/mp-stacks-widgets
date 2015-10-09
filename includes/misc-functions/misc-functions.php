9ae82a1aa7a7269fc186455f77f3a3ccef2604b2
Phil Johnston <support@mintplugins.com>
2015-10-08 22:15:50 -0400

    date change for copyright

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -4,10 +4,10 @@
  *
  * @since 1.0.0
  *
- * @package    MP Stacks Features
+ * @package    MP Stacks Widgets
  * @subpackage Functions
  *
- * @copyright  Copyright (c) 2014, Mint Plugins
+ * @copyright  Copyright (c) 2015, Mint Plugins
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @author     Philip Johnston
  */
