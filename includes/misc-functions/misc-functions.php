0f020a216c2f114c191da162aed193de76f2a48b
Phil Johnston <support@mintplugins.com>
2016-02-05 13:51:03 -0500

    Save Brick Sidebars in wp_options table permanantly
    Rather than removing the widgets from the wp_options table when editing a brick, we now only display the widget applied to the PHP session variable. This solves the issue of multiple brick widget areas making other ones dissapear sporatically

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -30,141 +30,71 @@ function mp_stacks_widgets_centered_by_default( $centered_content_types ){
 add_filter( 'mp_stacks_centered_content_types', 'mp_stacks_widgets_centered_by_default' );
 
 /**
- * Unregister all sidebars if the URL variable to unset them is set. 
+ * Set up the widget-control page that displays in the iframe for the Brick editor. Unregister all sidebars if the URL variable to unset them is set. 
  * This is so that after, we can register a single sidebar on the brick's edit screen so there is no confusion about which sidebar is the right one.
  *
  * @access   public
  * @since    1.0.0
  * @return   void
  */
-function mp_stacks_widgets_unregister_all_sidebars(){
+function mp_stacks_setup_brick_sidebars(){
 	
-	if ( !isset( $_GET['unregister_all_widgets'] ) ){
-		return;
-	}
-	
-	if ( !empty( $GLOBALS['wp_registered_sidebars'] ) ){
+	//If we should only register the Sidebar for the Brick being edited currently
+	if ( isset( $_GET['mp_stacks_widgets_setup_iframe_editor'] ) ){
+			
+		if( !session_id() )
+        session_start();				
 		
-		foreach( $GLOBALS['wp_registered_sidebars'] as $sidebar_id => $sidebar ){
-			unregister_sidebar( $sidebar_id );	
+		if ( !empty( $GLOBALS['wp_registered_sidebars'] ) ){
+						
+			//Unregister all other sidebars for now
+			foreach( $GLOBALS['wp_registered_sidebars'] as $sidebar_id => $sidebar ){
+				unregister_sidebar( $sidebar_id );	
+			}
+			
+			//Get the sidebar args for the Sidebar in this Brick
+			$current_sidebar = isset( $_SESSION['mp_stacks_widgets_current_sidebar'] ) ? $_SESSION['mp_stacks_widgets_current_sidebar'] : NULL;
+							
+			//If our sidebar args have been set
+			if ( !empty( $current_sidebar ) ){
+					
+					//If the sidebar id is empty, for backwards compatibility, use the brick title santizied	
+					if ( empty( $current_sidebar['id'] ) ){
+						$current_sidebar['id'] = sanitize_title( $current_sidebar['name'] );
+					}
+								
+					//Register the sidebar into this iframe so that we can edit it.
+					register_sidebar( $current_sidebar );
+				
+			}
+			
+		
 		}
-	
+		
+	}
+	//If we are not editing a Brick with a sidebar right now, register all the Brick Sidebars
+	else{
+		
+		//Get the registered sidebars
+		$mp_stacks_sidebars = get_option( 'mp_stacks_sidebar_args' );
+			
+		//If our sidebar args have been set
+		if ( !empty( $mp_stacks_sidebars ) ){
+			foreach( $mp_stacks_sidebars as $mp_stacks_sidebar_args ){
+				
+				//If the sidebar id is empty, for backwards compatibility, use the brick title santizied	
+				if ( empty( $mp_stacks_sidebar_args['id'] ) ){
+					$mp_stacks_sidebar_args['id'] = sanitize_title( $mp_stacks_sidebar_args['name'] );
+				}
+							
+				//register a sidebar for each brick that needs one
+				register_sidebar( $mp_stacks_sidebar_args );
+			}
+		}	
 	}
 	 
 }
-add_action( 'widgets_init', 'mp_stacks_widgets_unregister_all_sidebars', 100 );
- 
-/**
- * Register the sidebars we need
- *
- * @access   public
- * @since    1.0.0
- * @return   void
- */
-function mp_stacks_widgets_register_sidebars(){
-			
-	//Get the registered sidebars
-	$mp_stacks_sidebars = get_option( 'mp_stacks_sidebar_args' );
-		
-	//If our sidebar args have been set
-	if ( !empty( $mp_stacks_sidebars ) ){
-		foreach( $mp_stacks_sidebars as $mp_stacks_sidebar_args ){
-			
-			//If the sidebar id is empty, for backwards compatibility, use the brick title santizied	
-			if ( empty( $mp_stacks_sidebar_args['id'] ) ){
-				$mp_stacks_sidebar_args['id'] = sanitize_title( $mp_stacks_sidebar_args['name'] );
-			}
-						
-			//register a sidebar for each brick that needs one
-			register_sidebar( $mp_stacks_sidebar_args );
-		}
-	}
-	
-}
-add_action( 'widgets_init', 'mp_stacks_widgets_register_sidebars', 101 );
-
-/**
- * Generate Transients with info about what sidebars need to be generated
- *
- * @access   public
- * @since    1.0.0
- * @return   void
- */
-function mp_stacks_widgets_set_sidebars_transient(){
-	
-	if ( is_admin() ){
-		return;	
-	}
-	
-	//This makes this session variable current to this blog only - in case we are dealing witgh multiple widgetized bricks on multi-site wordpresses.	
-	$current_blog_id = 'site-' . get_current_blog_id();
-		
-	if ( isset( $_SESSION[$current_blog_id]['mp_stacks_widgets_refresh_on_next_frontendload'] ) && $_SESSION[$current_blog_id]['mp_stacks_widgets_refresh_on_next_frontendload'] ){
-		
-		unset( $_SESSION[$current_blog_id]['mp_stacks_widgets_refresh_on_next_frontendload'] );
-		
-		//Set the args for the new query
-		$mp_brick_args = array(
-			'post_type' => "mp_brick",
-			'posts_per_page' => -1,
-			'orderby' => 'menu_order',
-			'order' => 'ASC',
-		);	
-			
-		//Create new query for stacks
-		$mp_brick_query = new WP_Query( apply_filters( 'mp_brick_args', $mp_brick_args ) );
-		
-		$mp_stacks_sidebars = array();
-		
-		//Loop through all bricks
-		if ( $mp_brick_query->have_posts() ) { 
-			
-			while( $mp_brick_query->have_posts() ) : $mp_brick_query->the_post(); 
-				
-				$brick_id = get_the_ID(); 
-				
-				//First Media Type
-				$mp_stacks_first_content_type = get_post_meta($brick_id, 'brick_first_content_type', true);
-	
-				//Second Media Type
-				$mp_stacks_second_content_type = get_post_meta($brick_id, 'brick_second_content_type', true);
-					
-				//If a content type is set to be widgets
-				if ( $mp_stacks_first_content_type == 'widgets' || $mp_stacks_second_content_type == 'widgets' ){
-					
-					$title = "Brick: " . get_the_title( $brick_id );
-					//Sidebar ID
-					$sidebar_id = mp_core_get_post_meta( $brick_id, 'mp_stacks_widgets_brick_sidebar_id', 'none' );
-					
-					//If the sidebar id is empty, for backwards compatibility, use the brick title santizied
-					if ( $sidebar_id == 'none' ){
-						$sidebar_id = sanitize_title( $title );
-					}
-					
-					//Add it to the widgets transient where we store all brick widgets
-					$mp_stacks_sidebars[$sidebar_id] = array(
-						'name'          => $title,
-						'id'            => sanitize_title( $sidebar_id ),
-						'description'   => '',
-						'class'         => '',
-						'before_widget' => '<div class="mp-stacks-widgets-item">',
-						'after_widget'  => '</div>',
-						'before_title'  => '<div class="mp-stacks-widgets-title">',
-						'after_title'   => '</div>' 
-					);
-					
-				}
-	
-			endwhile;
-			
-			//This transient is used to register a sidebar for each brick that needs one
-			update_option( 'mp_stacks_sidebar_args', $mp_stacks_sidebars );
-				
-		}
-	}
-
-}
-add_action( 'after_setup_theme', 'mp_stacks_widgets_set_sidebars_transient' );
+add_action( 'widgets_init', 'mp_stacks_setup_brick_sidebars', 100 );
 
 /**
  * Ajax CallBack to Generate Transients with info about what sidebars need to be generated
@@ -177,14 +107,9 @@ function mp_stacks_ajax_set_sidebars_transient(){
 	
 	if( !session_id() )
         session_start();
-		
-	$_SESSION['site-' . get_current_blog_id()]['mp_stacks_widgets_refresh_on_next_frontendload'] = true;
-	
-	//Set the registered sidebars to be blank. We only need to show the sidebar for this brick on the next page load - which is all this affects.
-	$mp_stacks_sidebars = array();
 	
 	//Get the brick ID and set the name of the new sidebar
-	$post_id = $_POST['mp_stacks_widgets_brick_id'];			
+	$post_id = $_POST['mp_stacks_widgets_brick_id'];		
 	$title = "Brick: " . get_the_title( $post_id );
 	
 	//BACKWARDS COMPATIBILITY: If this brick was published before February 10, 2015, get the title, convert it to a slug, and use that for the sidebar id.
@@ -209,7 +134,10 @@ function mp_stacks_ajax_set_sidebars_transient(){
 		}
 	}
 	
-	//The the transient that we want a sidebar to be registered for this brick
+	//Get the existing MP Stacks + Widgets Sidebars saved into Bricks
+	$mp_stacks_sidebars = get_option( 'mp_stacks_sidebar_args' ); 
+	
+	//Add this Sidebar into the list of sidebars saved into Bricks
 	$mp_stacks_sidebars[$sidebar_id] = array(
 		'name'          => $title,
 		'id'            => sanitize_title( $sidebar_id ),
@@ -221,12 +149,15 @@ function mp_stacks_ajax_set_sidebars_transient(){
 		'after_title'   => '</div>' 
 	);	
 	
-	//Save the transient
+	//Save the MP Stacks + Widgets Sidebars saved into Bricks with this new one added.
 	update_option( 'mp_stacks_sidebar_args', $mp_stacks_sidebars );
+	
+	//Set the Session variable to let the iframe know which sidebar we want to edit.
+	$_SESSION['mp_stacks_widgets_current_sidebar'] = $mp_stacks_sidebars[$sidebar_id];
 	
 	//Give a "Manage Widgets" button to the meta field for the user to click since their sidebar will now be available 
 	echo json_encode( array( 
-		'iframe' => '<iframe src="' . mp_core_add_query_arg( array( 'mp-stacks-minimal-admin' => true, 'unregister_all_widgets' => true ), admin_url( 'widgets.php' ) ) . '" target="_blank" width="100%" height="500px" frameborder="0" scrolling="no" onload=\'javascript:mp_stacks_resizeIframe(this);\'>' . __( 'Manage Widgets', 'mp_stacks_widgets' ) . '</a>',
+		'iframe' => '<iframe src="' . mp_core_add_query_arg( array( 'mp-stacks-minimal-admin' => true, 'mp_stacks_widgets_setup_iframe_editor' => true ), admin_url( 'widgets.php' ) ) . '" target="_blank" width="100%" height="500px" frameborder="0" scrolling="no" onload=\'javascript:mp_stacks_resizeIframe(this);\'>' . __( 'Manage Widgets', 'mp_stacks_widgets' ) . '</a>',
 		'mp_stacks_widgets_brick_sidebar_id' => sanitize_title( $sidebar_id )
 		)
 	);
@@ -332,3 +263,79 @@ function m_stacks_widgets_end_session() {
 add_action('init', 'm_stacks_widgets_start_session', 1);
 add_action('wp_logout', 'm_stacks_widgets_end_session');
 add_action('wp_login', 'm_stacks_widgets_end_session');
+
+
+/**
+ * ReCheck all Bricks for sidebars and add those to the Option in the Options table containing all the Sidebars in Bricks.
+ *
+ * @access   public
+ * @since    1.0.0
+ * @return   void
+ */
+function mp_stacks_widgets_recheck_all_bricks_for_sidebars(){
+	
+	if ( !isset( $_GET['mp_stacks_widgets_recheck_all_bricks_for_sidebars'] ) ){
+		return;	
+	}
+					
+	//Set the args for the new query
+	$mp_brick_args = array(
+		'post_type' => "mp_brick",
+		'posts_per_page' => -1,
+		'orderby' => 'menu_order',
+		'order' => 'ASC',
+	);	
+		
+	//Create new query for stacks
+	$mp_brick_query = new WP_Query( apply_filters( 'mp_brick_args', $mp_brick_args ) );
+	
+	$mp_stacks_sidebars = array();
+	
+	//Loop through all bricks
+	if ( $mp_brick_query->have_posts() ) { 
+		
+		while( $mp_brick_query->have_posts() ) : $mp_brick_query->the_post(); 
+			
+			$brick_id = get_the_ID(); 
+			
+			//First Media Type
+			$mp_stacks_first_content_type = get_post_meta($brick_id, 'brick_first_content_type', true);
+
+			//Second Media Type
+			$mp_stacks_second_content_type = get_post_meta($brick_id, 'brick_second_content_type', true);
+				
+			//If a content type is set to be widgets
+			if ( $mp_stacks_first_content_type == 'widgets' || $mp_stacks_second_content_type == 'widgets' ){
+				
+				$title = "Brick: " . get_the_title( $brick_id );
+				//Sidebar ID
+				$sidebar_id = mp_core_get_post_meta( $brick_id, 'mp_stacks_widgets_brick_sidebar_id', 'none' );
+				
+				//If the sidebar id is empty, for backwards compatibility, use the brick title santizied
+				if ( $sidebar_id == 'none' ){
+					$sidebar_id = sanitize_title( $title );
+				}
+				
+				//Add it to the widgets transient where we store all brick widgets
+				$mp_stacks_sidebars[$sidebar_id] = array(
+					'name'          => $title,
+					'id'            => sanitize_title( $sidebar_id ),
+					'description'   => '',
+					'class'         => '',
+					'before_widget' => '<div class="mp-stacks-widgets-item">',
+					'after_widget'  => '</div>',
+					'before_title'  => '<div class="mp-stacks-widgets-title">',
+					'after_title'   => '</div>' 
+				);
+				
+			}
+
+		endwhile;
+		
+		//This transient is used to register a sidebar for each brick that needs one
+		update_option( 'mp_stacks_sidebar_args', $mp_stacks_sidebars );
+				
+	}
+
+}
+add_action( 'admin_init', 'mp_stacks_widgets_recheck_all_bricks_for_sidebars' );
